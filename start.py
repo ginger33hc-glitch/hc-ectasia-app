3de9067f2b8f6635069244b42744d657b79bc81b
ginger33hc-glitch <ginger33hc@gmail.com>
2026-08-27 14:34:12 -0400

    Start HC app through LASIK planning bootstrap

--- start.py
+++ start.py
@@ -3,4 +3,4 @@ import uvicorn
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", "8080"))
-    uvicorn.run("app:app", host="0.0.0.0", port=port)
+    uvicorn.run("bootstrap:app", host="0.0.0.0", port=port)
