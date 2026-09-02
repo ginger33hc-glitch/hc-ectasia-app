c4e9ac5fc6ad7258ce3d1ce5e94c5d6d41c90032
ginger33hc-glitch <ginger33hc@gmail.com>
2026-09-02 14:23:07 -0400

    Register canonical Pentacam fields needed by PS3

--- pentacam_field_registry.py
+++ pentacam_field_registry.py
@@ -7,7 +7,8 @@ defined in this module.
 
 TARGET_FIELDS = (
     "K1_D", "K1_axis_deg", "K2_D", "K2_axis_deg", "Kmax_D",
-    "corneal_diameter_mm", "pachy_thinnest_um", "central_pachy_um", "B_Ele_Th_um", "BAD_D", "Df", "Db",
+    "topographic_astig_D", "topographic_steep_axis_deg", "posterior_Kmean_D",
+    "corneal_diameter_mm", "pachy_thinnest_um", "central_pachy_um", "F_Ele_Th_um", "B_Ele_Th_um", "BAD_D", "Df", "Db",
     "Dp", "Dt", "Da", "PPI_avg", "PPI_min", "PPI_max", "ARTmax_um",
     "ISV", "IVA", "KI", "CKI", "IHD", "I_S", "KISA", "IHA",
     "Rmin_mm", "anterior_elevation_thinnest_um",
@@ -17,15 +18,24 @@ TARGET_FIELDS = (
 )
 
 CORNEA_FRONT_KERATOMETRY_SOURCE = "SHOW_2_EXAMS_TOPOMETRIC_CORNEA_FRONT"
+CORNEA_BACK_KERATOMETRY_SOURCE = "SHOW_2_EXAMS_TOPOMETRIC_CORNEA_BACK"
+PS3_TOPOGRAPHIC_INDEX_SOURCE = "SHOW_2_EXAMS_TOPOMETRIC_INDEX_BLOCK"
+BAD_THINNEST_ELEVATION_SOURCE = "BAD_DISPLAY_THINNEST_ELEVATION_BOXES"
+BAD_PROGRESSION_INDEX_SOURCE = "BAD_DISPLAY_PROGRESSION_INDEX"
+
 CORNEA_FRONT_KERATOMETRY_FIELDS = frozenset({
     "K1_D",
     "K1_axis_deg",
     "K2_D",
     "K2_axis_deg",
     "Kmean_D",
+    "topographic_astig_D",
+    "topographic_steep_axis_deg",
 })
+CORNEA_BACK_KERATOMETRY_FIELDS = frozenset({"posterior_Kmean_D"})
 KERATOMETRY_SOURCE_VALUES = (
     CORNEA_FRONT_KERATOMETRY_SOURCE,
+    CORNEA_BACK_KERATOMETRY_SOURCE,
     "OTHER_PENTACAM_SOURCE",
     "UNREADABLE",
     "NOT_SHOWN",
@@ -36,11 +46,16 @@ KERATOMETRY_SOURCE_VALUES = (
 # neighboring value. The first valid same-eye labeled-box transcription is retained.
 EXCLUSIVE_LABELED_BOX_FIELDS = frozenset({
     *CORNEA_FRONT_KERATOMETRY_FIELDS,
+    *CORNEA_BACK_KERATOMETRY_FIELDS,
     "Kmax_D",
     "ARTmax_um",
     "pachy_thinnest_um",
     "central_pachy_um",
+    "F_Ele_Th_um",
     "B_Ele_Th_um",
+    "PPI_avg",
+    "I_S",
+    "KISA",
 })
 
 COMPLETION_NUMERIC_FIELDS = {
@@ -51,14 +66,20 @@ COMPLETION_NUMERIC_FIELDS = {
     "Dp": "BAD Dp",
     "Dt": "BAD Dt",
     "Da": "BAD Da",
+    "F_Ele_Th_um": "F. Ele.Th (µm; BAD Display labeled box)",
+    "B_Ele_Th_um": "B. Ele.Th (µm; BAD Display labeled box)",
     "ARTmax_um": "ARTmax (µm)",
     "PPI_min": "PPI minimum",
     "PPI_avg": "PPI average",
     "PPI_max": "PPI maximum",
     "K1_D": "K1 (D)",
     "K2_D": "K2 (D; not Kmax)",
-    "Kmean_D": "Preoperative Kmean (D)",
+    "Kmean_D": "Anterior Kmean/Km (D; Cornea Front)",
+    "posterior_Kmean_D": "Posterior Kmean/Km (D; Cornea Back)",
+    "topographic_astig_D": "Topographic Astig. (D; Cornea Front)",
+    "topographic_steep_axis_deg": "Topographic Axis (steep) (degrees; Cornea Front)",
     "Kmax_D": "Kmax (D)",
+    "KISA": "KISA (%)",
     "srax_deg": "SRAX (degrees)",
     "inferior_opposite_steepening_D": "Inferior-opposite steepening (D)",
     "Rmin_mm": "Rmin (mm)",
